05c4c7865986609db0ae3d1d3d6f4103da20f524
dogrizz <dogrizz@ate.(none)>
2009-11-20 00:11:36 +0100

    tagowanie przy uzyciu acts as taggable on steroids

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ActiveRecord::Base
 	belongs_to :project
 	validates_presence_of :short, :task, :project_id, :priority
 	validates_length_of :short, :task, :minimum => 3
+
+	acts_as_taggable
+	
 end
